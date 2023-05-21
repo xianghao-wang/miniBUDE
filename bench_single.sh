00cac13a0215aff14a849398e63bc4676bac225a
xianghao <xianghaowang2001@outlook.com>
2023-05-21 22:41:40 +1000

    add single bench

---
+++ bench_single.sh
@@ -0,0 +1,29 @@
+# Usage
+# ./bench_single folder_name
+
+n=65536
+i=16
+WG=(64 128 256 512)
+
+folder=$(pwd)
+cd $1
+make clean
+make
+
+echo benchmarking $1
+
+out=${folder}/$(basename $(pwd)).txt
+echo "" > $out
+for wgsize in ${WG[@]}
+do
+  t=$n
+  for s in {1..8}
+  do
+    echo "" >> $out
+    echo ./bude -i $i -n $n -wgsize $wgsize >> $out
+    ./bude -i $i -n $n --wgsize $wgsize >> $out
+    echo "####################################################" >> $out
+    n=`expr $n \* 2`
+  done
+  n=$t
+done
